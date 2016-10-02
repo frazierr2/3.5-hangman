67c2967eafb53f1df0d96e6555fdddd78d1c0f8c
Ryan Frazier <frazierr2@gmail.com>
2016-10-01 22:01:46 -0400

    adding Players guess section

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
 (function(){
   'use strict';
   var wordsGreaterThanThree;
-  var randomWordGenerated;
-  var randomWord;
+  var generatedWord;
+  var word;
   var remaingGuesses;
+  var playerGuessButton;
+  var inputBox;
+  var playerGuess;
 
-//returning words that are more than 2 letters.
+
+remaingGuesses = 5;
+
+//Returning words that are more than 2 letters.
   wordsGreaterThanThree = commonWords.filter(function(words){
   return words.length > 2 && words.length < 10;
 });
 console.log(wordsGreaterThanThree); // Prints words in the group greater than or equal to 3.
 
+//Use the following to generate the word that is then put on screen.
+function wordGenerator(){
+  generatedWord = Math.floor(Math.random() * wordsGreaterThanThree.length);
+  return wordsGreaterThanThree[generatedWord];
+}
+
+word = wordGenerator();
+
+
+//Below is where we will set up when the player guesses and what to do.
+playerGuessButton = document.getElementById('button');
+button.addEventListener('click', playerInput);
+
+function playerInput(){
+  inputBox = document.getElementById('guess');
+  playerGuess = inputBox.value;
+  inputBox.value = "";
+  isLetterCorrect(playerGuess);
+  subtractLives();
+}
 
 
 
