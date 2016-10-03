ad1eed71a819b90694586c6fc18feb62490f7581
Ryan Frazier <frazierr2@gmail.com>
2016-10-02 20:31:28 -0400

    Changes to the whole program

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
   var generatedWord;
   var word;
   var remaingGuesses;
+  var wordSlots;
+  var characters;
   var playerGuessButton;
   var inputBox;
   var playerGuess;
@@ -22,37 +24,44 @@ console.log(wordsGreaterThanThree); // Prints words in the group greater than or
 function wordGenerator(){
   generatedWord = Math.floor(Math.random() * wordsGreaterThanThree.length);
   return wordsGreaterThanThree[generatedWord];
+  }
+word = wordGenerator();
+console.log(word); //Logs the random word with each refresh of the screen.
+
+
+//updating spaces
+wordSlots = document.querySelector(".your-word");
+for (var i = 0; i < word.length; i++) {
+    characters = document.createElement("span");
+    wordSlots.appendChild(characters);
 }
 
-word = wordGenerator();
-
-
-//Below is where we will set up when the player guesses and what to do.
-playerGuessButton = document.getElementById("button");
+// //Below is where we will set up when the player guesses and what to do.
+playerGuessButton = document.getElementById("guess");
 playerGuessButton.addEventListener("click", playerInput);
 
 function playerInput(){
-  inputBox = document.getElementById("guess");
+  inputBox = document.getElementById("text");
   playerGuess = inputBox.value;
   inputBox.value = "";
   isLetterCorrect(playerGuess);
   subtractLives();
 }
-
-//Is the letter correct?
-letter = 0;
- yourWord = document.querySelector(".your-word");
-function isLetterCorrect(playerGuess) {
-  for (var i = 0; i < word.length; i++) {
-    if(word[i] == playerGuess){
-      yourWord.textContent = playerGuess;
-      letter += 1;
-    }
-    if(letter == word.length){
-      banner.textContent = "WINNER WINNER CHICKEN DINNER";
-    }
-  }
-}
+//
+// //Is the letter correct?
+// letter = 0;
+//  yourWord = document.querySelector(".your-word");
+// function isLetterCorrect(playerGuess) {
+//   for (var i = 0; i < word.length; i++) {
+//     if(word[i] == playerGuess){
+//       yourWord.textContent = playerGuess;
+//       letter += 1;
+//     }
+//     if(letter == word.length){
+//       banner.textContent = "WINNER WINNER CHICKEN DINNER";
+//     }
+//   }
+// }
 
 
 
